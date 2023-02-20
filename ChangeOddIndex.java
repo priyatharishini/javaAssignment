f3d6f012a8001c727001a6989b28cf37bfeb1aa4
priyatharishini <61748229+priyatharishini@users.noreply.github.com>
2023-02-20 21:10:19 +0530

    Add files via upload
    javaassignment week3.day1 changeOddIndex even  uppercase

---
+++ ChangeOddIndex.java
@@ -0,0 +1,27 @@
+package javaassignment;
+
+import com.aventstack.extentreports.model.Test;
+
+public class ChangeOddIndex {
+
+	public static void main(String[] args) {
+		String test="changeme";
+char[] charArray = test.toCharArray();
+for(int i=0;i<charArray.length;i++)
+{
+	if(i%2==0)
+	{
+		
+charArray[i]=Character.toUpperCase(charArray[i]);
+System.out.println(charArray[i]);
+		
+	
+	}
+	else
+	{
+		
+		System.out.print("  "+charArray[i] +" ");
+	}
+}
+	}
+}
